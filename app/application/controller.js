ac2845306b7223adad41703703c0fea3ab069a28
Carlos Feliciano <c.feliciano2009@gmail.com>
2015-05-23 12:39:19 -0400

    Toggle modal with a bigger image when a cam is clicked.

--- app/application/controller.js
+++ app/application/controller.js
@@ -2,5 +2,15 @@ import Ember from 'ember';
 import Cams from '../utils/cams';
 
 export default Ember.Controller.extend({
-  cams: new Cams()
+  cams: new Cams(),
+  showingModal: false,
+  selectedCam: null,
+
+  actions: {
+    // Toggle the modal with the cam that the user selected.
+    toggleModal(cam) {
+      this.set('selectedCam', cam);
+      this.toggleProperty('showingModal');
+    }
+  }
 });
